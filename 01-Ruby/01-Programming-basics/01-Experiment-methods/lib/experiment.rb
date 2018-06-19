371d64d1106380047157a10b7d5a5b78459f4200
Rayan Castro <rayancdc@gmail.com>
2018-06-19 11:15:00 -0300

    Define a few methods

--- 01-Ruby/01-Programming-basics/01-Experiment-methods/lib/experiment.rb
+++ 01-Ruby/01-Programming-basics/01-Experiment-methods/lib/experiment.rb
@@ -3,28 +3,27 @@
 # - Just look in the doc for the right method of the String, Integer, and Array classes!
 
 def get_rid_of_surrounding_whitespaces(a_string)
-  # TODO: return a copy of the string with leading and trailing whitespaces removed
-  # example: get_rid_of_surrounding_whitespaces("  hey yo  ") => "hey yo"
+  return a_string.strip
 end
 
 def belongs_to?(a_string, a_word)
-  # TODO: return true if a_string contains a_word
-  # example: belongs_to?("hey jude", "jude") => true
+  return a_string.include? a_word
 end
 
 def replace(initial_string, old_letter, new_letter)
-  # TODO: return a copy of the string with the new letter replacing the old one
-  # example: replace("casanova", "a", "o") => "cosonovo"
+  return initial_string.tr(old_letter, new_letter)
 end
 
 def exactly_divide(an_integer, a_divider)
   # TODO: return divison of an_integer by a_divider (computing the floating division, NOT the euclidean one)
   # example: exactly_divide(13, 4) => 3.25
+  an_integer.fdiv(a_divider)
 end
 
 def divisible_by_two?(an_integer)
   # TODO: return true if an_integer is divisible by 2
   # example: divisible_by_two?(6) => true
+  return an_integer %2 == 0
 end
 
 def random_subset(an_array, sample_size)
