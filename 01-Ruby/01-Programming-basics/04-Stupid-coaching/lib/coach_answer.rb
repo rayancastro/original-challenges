d341b35e5d93e7beb1f5c73158dcd3ea64573038
Rayan Castro <rayancdc@gmail.com>
2018-06-19 15:01:13 -0300

    Solve the coach_answer_enhanced

--- 01-Ruby/01-Programming-basics/04-Stupid-coaching/lib/coach_answer.rb
+++ 01-Ruby/01-Programming-basics/04-Stupid-coaching/lib/coach_answer.rb
@@ -2,9 +2,7 @@ def coach_answer(your_message)
   # TODO: return coach answer to your_message
   if your_message == "I am going to work right now!"
     return ""
-  end
-
-  if your_message.strip.end_with?("?")
+  elsif your_message.strip.end_with?("?")
     return "Silly question, get dressed and go to work!"
   else
     return "I don't care, get dressed and go to work!"
@@ -15,8 +13,9 @@ def coach_answer_enhanced(your_message)
   # TODO: return coach answer to your_message, with additional custom rules of yours!
   if your_message == "I AM GOING TO WORK RIGHT NOW!"
     return ""
-  else if your_message.upcase?
-    puts "I can feel your motivation!"
+  elsif your_message.upcase == your_message
+    return "I can feel your motivation!" + " " + coach_answer(your_message).to_s
+  else
     return coach_answer(your_message)
   end
 end
