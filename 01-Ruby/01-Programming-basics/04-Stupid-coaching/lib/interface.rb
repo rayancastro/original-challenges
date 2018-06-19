3906738a155ef96a28402dd710a27c7f687fd33a
Rayan Castro <rayancdc@gmail.com>
2018-06-19 15:15:26 -0300

    Add interface interaction with user

--- 01-Ruby/01-Programming-basics/04-Stupid-coaching/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/04-Stupid-coaching/lib/interface.rb
@@ -1,2 +1,13 @@
 require_relative "coach_answer"
 # TODO: Implement the program that makes you discuss with your coach from the terminal.
+def ask_message
+  puts "Enter your message to the coach."
+  return gets.chomp.to_s
+end
+
+message = ask_message
+
+until coach_answer_enhanced(message).empty?
+  puts(coach_answer_enhanced(message))
+  message = ask_message
+end
