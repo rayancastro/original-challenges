24b67ef779ba2c5fe792d3723eeda9679f9fdca6
Rayan Castro <rayancdc@gmail.com>
2018-06-19 14:14:50 -0300

    Method compute name and interface done

--- 01-Ruby/01-Programming-basics/03-What-your-name/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/03-What-your-name/lib/interface.rb
@@ -5,4 +5,13 @@ require_relative "compute_name"
 # TODO: ask for the last name
 # TODO: Print a greeting to the user with the full name concatenated,
 #       something like "Hello, Boris Paillard"
+puts 'What\'s your first name?'
+first_name = gets.chomp.to_s
 
+puts 'What\'s your middle name?'
+middle_name = gets.chomp.to_s
+
+puts 'What\'s your last name?'
+last_name = gets.chomp.to_s
+
+puts(compute_name(first_name,middle_name,last_name))
