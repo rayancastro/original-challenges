7ca742fbe889cc3a8b49641c388ab14b2369f7ab
Rayan Castro <rayancdc@gmail.com>
2018-06-19 13:53:07 -0300

    correct order of arguments

--- 01-Ruby/01-Programming-basics/02-Age-in-days/lib/age_in_days.rb
+++ 01-Ruby/01-Programming-basics/02-Age-in-days/lib/age_in_days.rb
@@ -4,5 +4,5 @@ require 'date'
 def age_in_days(day, month, year)
   birthday = Date.new(year, month, day)
   current = Date.today
-  return (current.jd - birthday.jd)
+  return (current.jd - birthday.jd).to_i
 end
