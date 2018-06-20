423e28edd8374e68383b4289c4c546284249e3a3
Rayan Castro <rayancdc@gmail.com>
2018-06-20 11:26:05 -0300

    Solve horse format

--- 01-Ruby/02-Flow-Array/05-Destructive-Methods/lib/horse_racing.rb
+++ 01-Ruby/02-Flow-Array/05-Destructive-Methods/lib/horse_racing.rb
@@ -1,7 +1,7 @@
 def horse_racing_format!(race_array)
   # TODO: modify the given array so that it is horse racing consistent. This method should return nil.
+  race_array.collect!.with_index { |horse, index| (index + 1).to_s + "-" + horse }
+  race_array.reverse!
+  race_array.collect! { |horse| horse + "!" }
 end
-
-
-
 
