32681dad134a11d47c58b3e59498501135cea649
Rayan Castro <rayancdc@gmail.com>
2018-06-20 17:34:14 -0300

    Fix Beer Song Style issue

--- 01-Ruby/02-Flow-Array/Optional-03-Beer-song/lib/beer_song.rb
+++ 01-Ruby/02-Flow-Array/Optional-03-Beer-song/lib/beer_song.rb
@@ -1,15 +1,20 @@
-def beer_song(beer_start_count)
+def sing(beer_start_count)
   if beer_start_count == 2
     puts "#{beer_start_count} bottles of beer on the wall, #{beer_start_count} bottles of beer!"
     puts "Take one down, pass it around, #{beer_start_count - 1} bottle of beer on the wall!"
-    beer_song(beer_start_count - 1)
   elsif beer_start_count <= 1
     puts "#{beer_start_count} bottle of beer on the wall, #{beer_start_count} bottle of beer!"
     puts "Take one down, pass it around, no more bottles of beer on the wall!"
   else
     puts "#{beer_start_count} bottles of beer on the wall, #{beer_start_count} bottles of beer!"
     puts "Take one down, pass it around, #{beer_start_count - 1} bottles of beer on the wall!"
-    beer_song(beer_start_count - 1)
+  end
+end
+
+def beer_song(beer_start_count)
+  until beer_start_count < 1
+    sing(beer_start_count)
+    beer_start_count -= 1
   end
 end
 
