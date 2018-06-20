a4b97a109fe388dcde3b09b6b09dbf36a87dc5f8
Rayan Castro <rayancdc@gmail.com>
2018-06-20 17:25:38 -0300

    Solve recursive

--- 01-Ruby/02-Flow-Array/Optional-05-Simple-Looping-With-Recursion/lib/looping_with_recursion.rb
+++ 01-Ruby/02-Flow-Array/Optional-05-Simple-Looping-With-Recursion/lib/looping_with_recursion.rb
@@ -1,3 +1,9 @@
 def sum_recursive(min, max)
   # CONSTRAINT: you should use a recursive algorithm, i.e. the method should call itself
+  return -1 if min > max
+  if min == max
+    return max
+  else
+    return min + sum_recursive(min + 1, max)
+  end
 end
