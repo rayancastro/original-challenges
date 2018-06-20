a97ed7eb79966c79d646fbeadb998564a57fb995
Rayan Castro <rayancdc@gmail.com>
2018-06-20 10:49:00 -0300

    Implement interactive black jack play game

--- 01-Ruby/02-Flow-Array/03-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Flow-Array/03-Black-Jack/lib/croupier.rb
@@ -19,4 +19,21 @@ end
 def play_game
   # TODO: make the user play from terminal in a while loop that will stop
   #       when the user will not be asking for  a new card
+  player_score = 0
+  bank_score = pick_bank_score
+  loop do
+    puts "Card? 'y' or 'yes' to get a new card"
+    input = gets.chomp
+    if input == ('y' || 'yes')
+      player_score += pick_player_card
+      puts state_of_the_game(player_score, bank_score)
+      if player_score >= 21
+        puts end_game_message(player_score, bank_score)
+        break
+      end
+    else
+      puts end_game_message(player_score, bank_score)
+      break
+    end
+  end
 end
