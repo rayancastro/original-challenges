545c05d69d8507f4fc164cdfcdc9eaa33134c1da
Rayan Castro <rayancdc@gmail.com>
2018-06-20 10:28:27 -0300

    Solve croupier and black jack

--- 01-Ruby/02-Flow-Array/03-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Flow-Array/03-Black-Jack/lib/croupier.rb
@@ -1,13 +1,20 @@
 require_relative 'black_jack'
 
 def state_of_the_game(player_score, bank_score)
-  # TODO: return (not print!) a message containing the player's score and bank's score
+  return "Your score is #{player_score}, bank is #{bank_score}"
 end
 
 def end_game_message(player_score, bank_score)
-  # TODO: return (not print!) a message telling if the user won or lost.
+  if player_score > 21 || player_score < bank_score
+    return "You lost"
+  elsif player_score == bank_score
+    return "Push"
+  elsif player_score == 21
+    return "Black Jack"
+  else
+    return "You beat the bank! You win."
+  end
 end
-
 
 def play_game
   # TODO: make the user play from terminal in a while loop that will stop
