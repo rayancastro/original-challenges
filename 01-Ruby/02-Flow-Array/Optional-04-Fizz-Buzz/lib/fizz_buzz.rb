354ca09710124b08b0810f818fd0894103a85cd5
Rayan Castro <rayancdc@gmail.com>
2018-06-20 17:15:35 -0300

    Solve fizzbuzz

--- 01-Ruby/02-Flow-Array/Optional-04-Fizz-Buzz/lib/fizz_buzz.rb
+++ 01-Ruby/02-Flow-Array/Optional-04-Fizz-Buzz/lib/fizz_buzz.rb
@@ -1,3 +1,16 @@
 def fizz_buzz(number)
-  # TODO: return an array of integers, 'Fizz', 'Buzz' or 'FizzBuzz'
+  raise ArgumentError.new("Only positive non-zero numbers allowed") if number < 1
+  array = (1..number.to_i).to_a
+  array.map! do |e|
+    if (e % 3).zero? && (e % 5).zero?
+      "FizzBuzz"
+    elsif (e % 3).zero?
+      "Fizz"
+    elsif (e % 5).zero?
+      "Buzz"
+    else
+      e
+    end
+  end
+  array
 end
