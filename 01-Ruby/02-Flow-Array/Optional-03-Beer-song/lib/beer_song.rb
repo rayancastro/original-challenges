5312ea7bc075d58c649e8e13d5f1529711175913
Rayan Castro <rayancdc@gmail.com>
2018-06-20 16:54:40 -0300

    Solve Beer song

--- 01-Ruby/02-Flow-Array/Optional-03-Beer-song/lib/beer_song.rb
+++ 01-Ruby/02-Flow-Array/Optional-03-Beer-song/lib/beer_song.rb
@@ -1,3 +1,16 @@
 def beer_song(beer_start_count)
-  # TODO: sing the song
+  if beer_start_count == 2
+    puts "#{beer_start_count} bottles of beer on the wall, #{beer_start_count} bottles of beer!"
+    puts "Take one down, pass it around, #{beer_start_count - 1} bottle of beer on the wall!"
+    beer_song(beer_start_count - 1)
+  elsif beer_start_count <= 1
+    puts "#{beer_start_count} bottle of beer on the wall, #{beer_start_count} bottle of beer!"
+    puts "Take one down, pass it around, no more bottles of beer on the wall!"
+  else
+    puts "#{beer_start_count} bottles of beer on the wall, #{beer_start_count} bottles of beer!"
+    puts "Take one down, pass it around, #{beer_start_count - 1} bottles of beer on the wall!"
+    beer_song(beer_start_count - 1)
+  end
 end
+
+beer_song(ARGV[0].to_i)
