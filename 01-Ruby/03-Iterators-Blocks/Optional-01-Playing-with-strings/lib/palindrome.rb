ccefe90a4cf2c12ba52879edee3fc556414c79d0
Rayan Castro <rayancdc@gmail.com>
2018-06-21 12:55:52 -0300

    Add remove special method

--- 01-Ruby/03-Iterators-Blocks/Optional-01-Playing-with-strings/lib/palindrome.rb
+++ 01-Ruby/03-Iterators-Blocks/Optional-01-Playing-with-strings/lib/palindrome.rb
@@ -1,3 +1,11 @@
+ALPHABET = ("a".."z").to_a.join
+
+def remove_special(a_string)
+  regexp = Regexp.new("[^#{ALPHABET}]")
+  a_string.gsub!(regexp) { |element| "" }
+  return a_string
+end
+
 def palindrome?(a_string)
   # TODO: check if a_string is a palindrome
 end
