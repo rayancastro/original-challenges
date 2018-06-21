5b87418b607b2b8dc6bbecf91066b9e3642eb8ff
Rayan Castro <rayancdc@gmail.com>
2018-06-21 11:36:52 -0300

    Remove instruction comments

--- 01-Ruby/03-Iterators-Blocks/02-About-blocks/lib/block_gym.rb
+++ 01-Ruby/03-Iterators-Blocks/02-About-blocks/lib/block_gym.rb
@@ -6,7 +6,6 @@ def timer_for
 end
 
 def my_map(array)
-  # TODO: Re-implement the same behavior as `Enumerable#map` without using it! You can use `#each` though.
   arr = []
   array.each do |e|
     arr.push(yield(e))
@@ -15,8 +14,6 @@ def my_map(array)
 end
 
 def tag(tag_name, attributes = nil)
-  # TODO: Build HTML tags around content given in the block
-  #       The method can be called with an optional HTML attribute inputted as [attr_name, attr_value]
   if !attributes.nil?
     att = " " + attributes[0] + '="' + attributes[1] + '"'
     return "<#{tag_name}" + att + ">" + yield + "</#{tag_name}>"
