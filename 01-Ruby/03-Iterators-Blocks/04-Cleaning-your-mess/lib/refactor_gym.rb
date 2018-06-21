6610877b8e8490e54d02727bbf7f25f07110ba99
Rayan Castro <rayancdc@gmail.com>
2018-06-20 21:42:35 -0300

    Major refactor hop code

--- 01-Ruby/03-Iterators-Blocks/04-Cleaning-your-mess/lib/refactor_gym.rb
+++ 01-Ruby/03-Iterators-Blocks/04-Cleaning-your-mess/lib/refactor_gym.rb
@@ -1,17 +1,9 @@
-# Very dirty code that needs refactoring...
 def hop_hop_hop(number_of_exercises)
-  for i in (1..number_of_exercises) do
-    counter = 0
-    until counter == i
+  (1..number_of_exercises).each do |i|
+    (1..i).each do
       print "hop! "
-      counter += 1
     end
-    unless counter != i # Test if reaching the end of the current exercise.
-      # Prints message in that case
-      print 'One more time...' + "\n"
-    end
-    # Reset counter to 0 for the next exercise
-    counter = 0
+    print 'One more time...' + "\n"
   end
 end
 
