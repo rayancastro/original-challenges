6827cbd7e5e3c3d739f276a9b4f5366692837a24
Rayan Castro <rayancdc@gmail.com>
2018-06-21 11:31:27 -0300

    Solve style issues

--- 01-Ruby/03-Iterators-Blocks/05-Louchebem/lib/louchebem.rb
+++ 01-Ruby/03-Iterators-Blocks/05-Louchebem/lib/louchebem.rb
@@ -1,27 +1,30 @@
 VOGALS = "aeiou"
 CONSONANTS = ("a".."z").to_a.join.delete VOGALS
-SPECIAL = "?<>',?[]}{=-).(*&^%$#`~{}!"
+SPECIAL = "?<>',?[]}{=-).(*&^%$#`~{}! "
 SUFFIXES = ["em", "é", "ji", "oc", "ic", "uche", "ès"]
 
-def is_word?(word)
+def word?(word)
   SPECIAL.split("").each do |char|
     return false if word.include? char
   end
   return true
 end
 
+def first_consonant_group(word)
+  consonant_group = []
+  word.split("").each do |char|
+    break if VOGALS.include? char
+    raise ArgumentError.new, "Detected a symbol that should't be here." if SPECIAL.include? char
+    consonant_group.push(char) if CONSONANTS.downcase.include? char
+  end
+  return consonant_group
+end
+
 def louche_word(word)
-  # check if it is a space
-  return word if word == " "
   # check if it is a word, if not leave unchanged
-  if is_word?(word)
-    consonant_group = []
+  if word?(word)
     # checks first letters of word until it matches a vogal and store those letters.
-    word.split("").each do |char|
-      break if VOGALS.include? char
-      raise ArgumentError.new,"Detected a symbol that should't be here." if SPECIAL.include? char
-      consonant_group.push(char) if CONSONANTS.downcase.include? char
-    end
+    consonant_group = first_consonant_group(word)
     # check if the word is all consonants. returns word
     return word if consonant_group.size == word.size
     # remove those letters from word
@@ -31,7 +34,7 @@ def louche_word(word)
     # add stored letters to the end of the word
     consonant_group.each { |char| word += char }
     # add a sufix
-    word += SUFFIXES[rand(0..SUFFIXES.size-1)]
+    word += SUFFIXES[rand(0..SUFFIXES.size - 1)]
   end
   return word
 end
@@ -46,4 +49,4 @@ def louchebemize(sentence)
   return louche_array.join
 end
 
-p louchebemize("chat")
+p louchebemize("lets try this sentence")
