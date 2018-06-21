5fbd95667284ca20e216dcdd404269cbe29be3b5
Rayan Castro <rayancdc@gmail.com>
2018-06-21 16:41:12 -0300

    Solve burguer calories problem

--- 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
+++ 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
@@ -1,3 +1,15 @@
+MCDONALDS_CALORIES = {
+  "Hamburguer" => 250,
+  "Cheese Burger" => 300,
+  "Big Mac" => 540,
+  "McChicken" => 350,
+  "French Fries" => 230,
+  "Salad" => 15,
+  "Coca Cola" => 150,
+  "Sprite" => 150
+}
+
 def poor_calories_counter(burger, side, beverage)
   # TODO: return number of calories for this mcDonald order
+  return MCDONALDS_CALORIES[burger] + MCDONALDS_CALORIES[side] + MCDONALDS_CALORIES[beverage]
 end
