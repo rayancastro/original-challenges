7acbe6a684995ecf2c375e60629ed33160e65a72
Rayan Castro <rayancdc@gmail.com>
2018-06-21 14:20:33 -0300

    Solve procs

--- 01-Ruby/03-Iterators-Blocks/Optional-02-Storing-blocks-in-Proc/lib/love_message.rb
+++ 01-Ruby/03-Iterators-Blocks/Optional-02-Storing-blocks-in-Proc/lib/love_message.rb
@@ -1,18 +1,22 @@
 def tell(who, &message_blk)
   # TODO: return message addressed to who
+  return "#{who}, #{yield}!"
 end
 
 def tell_mum(&message_blk)
   # TODO: return message addressed to your mum
   # => Should call #tell of course
+  return "mum, #{yield}!"
 end
 
 def tell_with_proc(who, message_blk)
   # TODO: return message addressed to who
   # => Message is passed as Proc, not as block
+  return "#{who}, #{message_blk.call}!"
 end
 
 def tell_mum_with_proc(message_blk)
   # TODO: return message addressed to your mum
   # => Should call #tell_with_proc of course
+  return "mum, #{message_blk.call}!"
 end
