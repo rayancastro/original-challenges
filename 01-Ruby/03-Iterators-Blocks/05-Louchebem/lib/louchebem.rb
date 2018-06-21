58c95c53ade3e5b1f6385a9f0318b59576ea9477
Rayan Castro <rayancdc@gmail.com>
2018-06-21 11:17:03 -0300

    Solve louchebem

--- 01-Ruby/03-Iterators-Blocks/05-Louchebem/lib/louchebem.rb
+++ 01-Ruby/03-Iterators-Blocks/05-Louchebem/lib/louchebem.rb
@@ -1,33 +1,49 @@
+VOGALS = "aeiou"
+CONSONANTS = ("a".."z").to_a.join.delete VOGALS
+SPECIAL = "?<>',?[]}{=-).(*&^%$#`~{}!"
+SUFFIXES = ["em", "é", "ji", "oc", "ic", "uche", "ès"]
+
 def is_word?(word)
-  special = "?<>',?[]}{=-).(*&^%$#`~{}!"
-  special.split("").each do |char|
+  SPECIAL.split("").each do |char|
     return false if word.include? char
   end
   return true
 end
 
-def louchebemize(sentence)
-  suffixes = ["em", "é", "ji", "oc", "ic", "uche", "ès"]
-  word_array = sentence.split(/\b/)
-
-  # for each word
-  word_array.each do |word|
-    # check if it is a word, if not leave unchanged
-    if is_word?(word)
-      # checks first letters of word until it matches a vogal
-        word.split("").each do |char|
-          break if char.include
-
-      # store those letters
-      # remove thos letters
-      # add letter L to beggining of word
-      # add stored letters to the end of the word
-      # add a sufix
+def louche_word(word)
+  # check if it is a space
+  return word if word == " "
+  # check if it is a word, if not leave unchanged
+  if is_word?(word)
+    consonant_group = []
+    # checks first letters of word until it matches a vogal and store those letters.
+    word.split("").each do |char|
+      break if VOGALS.include? char
+      raise ArgumentError.new,"Detected a symbol that should't be here." if SPECIAL.include? char
+      consonant_group.push(char) if CONSONANTS.downcase.include? char
     end
-
+    # check if the word is all consonants. returns word
+    return word if consonant_group.size == word.size
+    # remove those letters from word
+    word = word.sub(consonant_group.join, "")
+    # add letter L to beggining of word
+    word = "l" + word
+    # add stored letters to the end of the word
+    consonant_group.each { |char| word += char }
+    # add a sufix
+    word += SUFFIXES[rand(0..SUFFIXES.size-1)]
   end
-
-
+  return word
 end
 
-p is_word?("abcde.")
+def louchebemize(sentence)
+  word_array = sentence.split(/\b/)
+  louche_array = []
+  # for each word
+  word_array.each do |word|
+    louche_array.push(louche_word(word))
+  end
+  return louche_array.join
+end
+
+p louchebemize("chat")
