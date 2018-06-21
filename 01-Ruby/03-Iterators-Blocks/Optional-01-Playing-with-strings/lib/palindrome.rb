92f03de5e62b0e4a3e8ad930927d0898d4612fe3
Rayan Castro <rayancdc@gmail.com>
2018-06-21 13:02:36 -0300

    Solve palindrome]

--- 01-Ruby/03-Iterators-Blocks/Optional-01-Playing-with-strings/lib/palindrome.rb
+++ 01-Ruby/03-Iterators-Blocks/Optional-01-Playing-with-strings/lib/palindrome.rb
@@ -2,10 +2,12 @@ ALPHABET = ("a".."z").to_a.join
 
 def remove_special(a_string)
   regexp = Regexp.new("[^#{ALPHABET}]")
-  a_string.gsub!(regexp) { |element| "" }
+  a_string.gsub!(regexp) { "" }
   return a_string
 end
 
 def palindrome?(a_string)
-  # TODO: check if a_string is a palindrome
+  return false if a_string == ""
+  final_string = remove_special(a_string.downcase)
+  return final_string == final_string.reverse
 end
