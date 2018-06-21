6f6cc5f41935edf48bf55589eecc13bc58b2cc32
Rayan Castro <rayancdc@gmail.com>
2018-06-21 20:21:10 -0300

    Solve problem using reduce and Hashs

--- 01-Ruby/04-Hash-Symbols/02-Modeling-with-Hash-part-II/lib/mc_donald.rb
+++ 01-Ruby/04-Hash-Symbols/02-Modeling-with-Hash-part-II/lib/mc_donald.rb
@@ -9,10 +9,24 @@ DISHES_CALORIES = {
   "Sprite" => 150
 }
 
+MEALS = {
+  "Happy Meal" => ["Cheese Burger", "French Fries", "Coca Cola"],
+  "Best Of Big Mac" => ["Big Mac", "French Fries", "Coca Cola"],
+  "Best Of McChicken" => ["McChicken", "Salad", "Sprite"]
+}
+
 def poor_calories_counter(burger, side, beverage)
   DISHES_CALORIES[burger] + DISHES_CALORIES[side] + DISHES_CALORIES[beverage]
 end
 
 def calories_counter(orders)
-  # TODO: return number of calories for a less constrained order
+  orders.reduce(0) do |sum, order|
+    if MEALS.key?(order)
+      sum + MEALS[order].reduce(0) { |memo, suborder| memo + DISHES_CALORIES[suborder] }
+    else
+      sum + DISHES_CALORIES[order]
+    end
+  end
 end
+
+p calories_counter(["Best Of Big Mac", "Salad", "Happy Meal", "Sprite"])
