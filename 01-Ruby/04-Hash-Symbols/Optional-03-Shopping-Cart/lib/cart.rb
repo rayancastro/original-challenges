c2faf62f0e274d0685c0cb0c2014af21ff4f1323
Rayan Castro <rayancdc@gmail.com>
2018-06-22 13:27:30 -0300

    implement most methods in cart and store

--- 01-Ruby/04-Hash-Symbols/Optional-03-Shopping-Cart/lib/cart.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-03-Shopping-Cart/lib/cart.rb
@@ -3,14 +3,24 @@
 
 def add_to_cart(cart, product)
   # TODO: you get a cart and a product. Add the product to the cart!
+  cart << product
 end
 
 def cart_to_s(cart)
   # TODO: you get a cart, return a coma-separated String list of the contained products
+  cart.join(',')
 end
 
 def cart_total_price(cart, store_items)
   # TODO: you get a cart and the store items with their prices. Return the total
   # price of the shopping cart.
   # /!\ Make sure it does not crash with carts having items not in store!
+  sum = 0
+  # For each word, check for hash corresponding value. adds to sum
+  cart.each do |item|
+    if store_items.key?(item.to_sym)
+      sum += store_items[item.to_sym]
+    end
+  end
+  return sum
 end
--- 01-Ruby/04-Hash-Symbols/Optional-03-Shopping-Cart/lib/store.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-03-Shopping-Cart/lib/store.rb
@@ -1,27 +1,33 @@
 # This file contains methods related to your store and its supplies.
 # You should complete it according to the instructions in the TODO comments
 
+
 def store_items
   # Here's what you should have in store:
-  #   yogurts: 2€
-  #   meat: 7€
-  #   vegetables: 5€
-  #   potatoes: 2€
-  #   rice: 1€
+    # yogurts: 2€
+    # meat: 7€
+    # vegetables: 5€
+    # potatoes: 2€
+    # rice: 1€
 
   # TODO: find the correct data structure to hold the shopping store items
   # instead of using nil below
-  store = nil
-
-  return store
+  { yogurts: 2,
+    meat: 7,
+    vegetables: 5,
+    potatoes: 2,
+    rice: 1 }
 end
 
 def product_in_store?(product)
   # TODO: Check whether product is present in the store items
+  store_items.key?(product.to_sym)
 end
 
 def price_of_product(product)
   # TODO: Return the price of the given product, if it's in store. nil otherwise
+  return store_items[product.to_sym] if store_items.key?(product.to_sym)
+  return nil
 end
 
 def store_items_to_s
@@ -31,4 +37,5 @@ def store_items_to_s
   # ...
   #
   # HINT: add line breaks in your String with "\n"
+  store_items.map { |key, value|  }
 end
