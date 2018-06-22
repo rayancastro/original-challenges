3b9c3cf4cba8d4c25079757c1d3c9fcd73f437bd
Rayan Castro <rayancdc@gmail.com>
2018-06-22 11:58:15 -0300

    Solve group anagrams problem

--- 01-Ruby/04-Hash-Symbols/Optional-02-Combine-anagrams/lib/group_anagrams.rb
+++ 01-Ruby/04-Hash-Symbols/Optional-02-Combine-anagrams/lib/group_anagrams.rb
@@ -1,3 +1,20 @@
 def group_anagrams(words)
   # TODO: group anagrams
+  # Create empty hash
+  anagrams = {}
+  # Iterate over the array of strings
+  words.each do |word|
+    # Store sorted version of word (for computational purposes)
+    sorted_word = word.downcase.chars.sort.join
+    # For each string, check hash for its sorted version as key:
+    if anagrams.key?(sorted_word)
+      # If the dictionary does have the key, push string to the value of the key (wich will be an array)
+      anagrams[sorted_word].push(word)
+    else
+      # If the dictionary doesnt have the key, add new key to hash with value equal to an array containing that string
+      anagrams[sorted_word] = [word]
+    end
+  end
+  # Return array of values from the dictionary
+  return anagrams.values
 end
