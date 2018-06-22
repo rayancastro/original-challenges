ea68f6a0473d7e6380fd6b1231421f612e8969b4
Rayan Castro <rayancdc@gmail.com>
2018-06-22 17:34:56 -0300

    Solve anagrams

--- 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
+++ 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
@@ -1,7 +1,39 @@
+ALPHABET = ("a".."z").to_a.join
+
 def anagrams?(a_string, another_string)
   # TODO: implement the obvious method to test if two words are anagrams
+  first_anagram = a_string.downcase.gsub(/[^#{ALPHABET}]/) { "" }.chars.sort.join
+  second_anagram = another_string.downcase.gsub(/[^#{ALPHABET}]/) { "" }.chars.sort.join
+  first_anagram == second_anagram
 end
 
 def anagrams_on_steroids?(a_string, another_string)
-  # TODO: implement the improved method
+  anagram_hash = {}
+  anagram_hash[a_string.downcase.gsub(/[^#{ALPHABET}]/) { "" }.chars.sort.join] = 1
+  anagram_hash.key?(another_string.downcase.gsub(/[^#{ALPHABET}]/) { "" }.chars.sort.join)
 end
+
+# def test_anagrams(argument_one, argument_two, iterations)
+#   sum = 0
+#   iterations.times do
+#     start = Time.now
+#     anagrams?(argument_one, argument_two)
+#     finish = Time.now
+#     sum += finish - start
+#   end
+#   return "This method spent %f seconds to computate, on average, after #{iterations} iterations." % (sum / iterations)
+# end
+
+# def test_anagrams_on_steroids(argument_one, argument_two, iterations)
+#   sum = 0
+#   iterations.times do
+#     start = Time.now
+#     anagrams_on_steroids?(argument_one, argument_two)
+#     finish = Time.now
+#     sum += finish - start
+#   end
+#   return "This method spent %f seconds to computate, on average, after #{iterations} iterations." % (sum / iterations)
+# end
+
+# puts test_anagrams("Monica Lewinsky!", "Nice silky woman", 100000)
+# puts test_anagrams_on_steroids("Monica Lewinsky!", "Nice silky woman", 100000)
