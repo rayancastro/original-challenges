b1413669bc3b07a2b19f8853db2c073f51091ed8
Rayan Castro <rayancdc@gmail.com>
2018-06-22 09:56:18 -0300

    Solve array to block

--- 01-Ruby/04-Hash-Symbols/05-Array-to-hash/lib/array_to_hash.rb
+++ 01-Ruby/04-Hash-Symbols/05-Array-to-hash/lib/array_to_hash.rb
@@ -1,3 +1,13 @@
 def array_to_hash(array)
-  # TODO: implement the method :)
+  hash_array = {}
+  array.each_with_index do |value, index|
+    if block_given?
+      hash_array[yield(index).to_s] = value
+    else
+      hash_array[index.to_s] = value
+    end
+  end
+  return hash_array
 end
+
+
