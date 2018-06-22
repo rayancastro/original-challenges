65ef243da02078ad024797c17f2d1e81a76c3795
Rayan Castro <rayancdc@gmail.com>
2018-06-22 15:08:43 -0300

    Solve Check phone

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,3 +1,10 @@
 def french_phone_number?(phone_number)
-  # TODO: true or false?
+  num_string = phone_number.gsub(/[ -]/) { "" }
+  return false if num_string[1] == "0"
+  (num_string[0] == "0" && num_string.size == 10) || (num_string[0..2] == "+33" && num_string.size == 12)
 end
+
+# p french_phone_number?("+33 6 65 36 36 36")
+# p french_phone_number?("0665363636")
+# p french_phone_number?("06-65-36-36-36")
+# p french_phone_number?("06 65 36 36")
