b656c17c6ebc3eaf1f01e7a82cb36ea8cd8db0a5
Rayan Castro <rayancdc@gmail.com>
2018-06-24 20:16:06 -0300

    Solve translation

--- 01-Ruby/05-Regular-Expressions/Optional-02-A-translation-story/lib/translation.rb
+++ 01-Ruby/05-Regular-Expressions/Optional-02-A-translation-story/lib/translation.rb
@@ -19,6 +19,24 @@ STRINGS = {
   }
 }
 
-def translation(a_string, a_language)
+def translation(a_string, a_language = "en")
   # TODO: your code goes here
+  path = a_string.split(".").map { |word| word.to_sym }
+  begin
+    if path.size == 2
+      if STRINGS[path[0]][path[1]].key?(a_language.to_sym)
+        return STRINGS[path[0]][path[1]][a_language.to_sym]
+      else return STRINGS[path[0]][path[1]][:en]
+      end
+    elsif path.size == 3
+      if STRINGS[path[0]][path[1]][path[2]].key?(a_language.to_sym)
+        return STRINGS[path[0]][path[1]][path[2]][a_language.to_sym]
+      else return STRINGS[path[0]][path[1]][path[2]][:en]
+      end
+    end
+  rescue
+    return ""
+  end
 end
+
+p translation('home.content.goodbye', 'de')
