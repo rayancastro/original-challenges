2cde9e137f35a6626e5759aaec40c395038e6959
Rayan Castro <rayancdc@gmail.com>
2018-06-24 17:27:51 -0300

    Implement method of word frequency

--- 01-Ruby/05-Regular-Expressions/04-Word-frequency/lib/word_frequency.rb
+++ 01-Ruby/05-Regular-Expressions/04-Word-frequency/lib/word_frequency.rb
@@ -1,3 +1,24 @@
+ALPHABET = (("a".."z").to_a + ("A".."Z").to_a).join
+def noise_hash(stop_words_file_name)
+  noise = { "" => 1 }
+  File.open(stop_words_file_name, "r").each_line do |line|
+    line.split.each { |word| noise[word] = 1 }
+  end
+  return noise
+end
+
 def most_common_words(file_name, stop_words_file_name, number_of_word)
-  # TODO: return hash of occurences of number_of_word most frequent words
+  noise = noise_hash(stop_words_file_name)
+  frequencies = {}
+  # Open the file and read each line
+  File.open(file_name, "r").each_line do |line|
+    # Separate the words
+    line.split(/[^#{ALPHABET}]/).each do |word|
+      word.downcase!
+      # Get rid of the noise, and for each word, add 1 to its value at the frequencies hash
+      (frequencies.key?(word) ? frequencies[word] += 1 : frequencies[word] = 1) unless noise.key?(word)
+    end
+  end
+  # Return a hash with the number_of_word most frequent words
+  frequencies.select { |_key, value| value >= frequencies.values.sort[number_of_word * -1] }
 end
