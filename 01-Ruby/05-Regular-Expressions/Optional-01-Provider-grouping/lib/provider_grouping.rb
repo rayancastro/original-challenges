6f76e2dfc9442df03547312c4921f47707783818
Rayan Castro <rayancdc@gmail.com>
2018-06-24 18:39:18 -0300

    Implement mail functions

--- 01-Ruby/05-Regular-Expressions/Optional-01-Provider-grouping/lib/provider_grouping.rb
+++ 01-Ruby/05-Regular-Expressions/Optional-01-Provider-grouping/lib/provider_grouping.rb
@@ -1,7 +1,28 @@
 def group_mails(emails)
   # TODO: group email by provider
+  domain_names = {}
+  emails.each do |email|
+    mail_array = email.split("@")
+    # user = mail_array[0]
+    domain = mail_array[1][0, mail_array[1].index(".")]
+    domain_names.key?(domain) ? domain_names[domain].push(email) : domain_names[domain] = [email]
+  end
+  return domain_names
 end
 
 def provider?(email, provider)
-  # TODO: return true if email is of given provider
+  mail_array = email.split("@")
+  domain = mail_array[1][0, mail_array[1].index(".")]
+  domain == provider
 end
+
+users = [
+  "bob@yahoo.fr",
+  "roger57@hotmail.fr",
+  "bigbox@yahoo.fr",
+  "boris@lewagon.org",
+  "monsieur.olivier@gmail.com",
+  "monsieur.mack@gmail.com"
+]
+
+p group_mails(users)
