2329d179923d579ed90ac1257db095e0d6d426c1
Rayan Castro <rayancdc@gmail.com>
2018-06-27 14:17:44 -0300

    Solve student

--- 02-OOP/01-OO-Basics/Optional-01-Richest-Student/lib/student.rb
+++ 02-OOP/01-OO-Basics/Optional-01-Richest-Student/lib/student.rb
@@ -1,2 +1,19 @@
 class Student
+  attr_reader :name
+
+  include Comparable
+  def <=>(other)
+    wealth <=> other.wealth
+  end
+
+  def initialize(name, fives, tens, twenties)
+    @name = name
+    @fives = fives
+    @tens = tens
+    @twenties = twenties
+  end
+
+  def wealth
+    return @fives * 5 + @tens * 10 + @twenties * 20
+  end
 end
