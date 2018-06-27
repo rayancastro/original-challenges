08ea589cb29a47ed71c8aaa36876c51242ccd410
Rayan Castro <rayancdc@gmail.com>
2018-06-27 20:18:06 -0300

    Solve restaurant

--- 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
@@ -1,9 +1,32 @@
 class Restaurant
   # TODO: add relevant accessors if necessary
+  attr_reader :name, :city, :average_rating
 
   def initialize(city, name)
     # TODO: implement constructor with relevant instance variables
+    @city = city
+    @name = name
+    @average_rating = 0.0
+    @number_of_rates = 0
   end
 
+  def rate(new_rate)
+    @average_rating  *= @number_of_rates
+    @number_of_rates += 1
+    @average_rating = (@average_rating + new_rate) / @number_of_rates
+  end
+
+  def self.filter_by_city(restaurants, city)
+    filtered = []
+    restaurants.each do |restaurant|
+      filtered.push(restaurant) if restaurant.city == city
+    end
+    return filtered
+  end
   # TODO: implement .filter_by_city and #rate methods
 end
+
+jules_verne = Restaurant.new("paris", "Jules Verne")
+
+p jules_verne.name
+p jules_verne.city
