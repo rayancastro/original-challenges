ee5b9db3532d3043f2554bc17add4ae152de0e19
Rayan Castro <rayancdc@gmail.com>
2018-06-27 11:46:34 -0300

    Solve bank challenge

--- 02-OOP/01-OO-Basics/04-Public-Interface/lib/bank_account.rb
+++ 02-OOP/01-OO-Basics/04-Public-Interface/lib/bank_account.rb
@@ -1,8 +1,12 @@
+require_relative 'transaction.rb'
+
 # This is how you define your own custom exception classes
 class DepositError < StandardError
 end
 
 class BankAccount
+  attr_reader :name
+  attr_reader :balance
   # Contract for the BankAccount class
   # - you can access full owner's name and balance, but partial IBAN
   # - you cannot access full IBAN
@@ -28,25 +32,52 @@ class BankAccount
   def withdraw(amount)
     # TODO: Call add_transaction with the right argument
     # TODO: returns a string with a message
+    raise DepositError if amount > @balance
+    add_transaction(-amount)
+    return "You withdraw $#{format('%.2f', amount)}."
   end
 
   def deposit(amount)
     # TODO: Call add_transaction with the right argument
     # TODO: returns a string with a message
+    add_transaction(amount)
+    return "You deposited $#{format('%.2f', amount)}."
   end
 
-  def transactions_history(args = {})
+  def password_check(password)
+    if !@password.empty?
+      return password == @password
+    else
+      return true
+    end
+  end
+
+  def transactions_history(args = { password: "" })
     # TODO: Check if there is a password and if so if it is correct
     # TODO: return a string displaying the transactions, BUT NOT return the transaction array!
+    return "no password given" if args[:password] == ""
+    if password_check(args[:password])
+      transaction_str = "------Transaction history---------\n"
+      @transactions.each do |transaction|
+        transaction_str = transaction.to_s + "\n" + "----------------------------------\n"
+      end
+      return transaction_str
+    else
+      return "wrong password"
+    end
   end
 
   def iban
+    data = @iban.match(/^(?<first>.{4})-(?<hide>.+)-(?<last>.+)$/)
+    partial_iban = data[:first] + data[:hide].delete("-").gsub(/./, "*") + data[:last]
+    return partial_iban
     # TODO: Hide the middle of the IBAN like FR14**************606 and return it
   end
 
   def to_s
     # Method used when printing account object as string (also used for string interpolation)
     # TODO: return a string with the account owner, the hidden iban and the balance of the account
+    return "Account Owner: #{@name}\n IBAN: #{iban}\n Balance: #{@balance}"
   end
 
   private
@@ -54,5 +85,8 @@ class BankAccount
   def add_transaction(amount)
     # TODO: add the amount in the transactions array
     # TODO: update the current balance (which represents the balance of the account)
+    @transactions.push(Transaction.new(amount))
+
+    @balance += amount
   end
 end
--- 02-OOP/01-OO-Basics/04-Public-Interface/lib/transaction.rb
+++ 02-OOP/01-OO-Basics/04-Public-Interface/lib/transaction.rb
@@ -2,9 +2,12 @@
 class Transaction
   def initialize(amount)
     # Initialize amount and date of transaction
+    @amount = amount
+    @time = Time.now
   end
 
   def to_s
     # Nicely print transaction info using Time#strftime.
+    return "Transaction of $#{@amount} at #{@time.strftime('%A, %B %d, %H:%M:%S.')}"
   end
 end
