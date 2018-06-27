7045ca3b0c8766f630c34f01dc9e6b706d009c65
Rayan Castro <rayancdc@gmail.com>
2018-06-27 09:19:58 -0300

    Fix style issue

--- 02-OOP/01-OO-Basics/01-Define-your-own-class/lib/interface.rb
+++ 02-OOP/01-OO-Basics/01-Define-your-own-class/lib/interface.rb
@@ -2,3 +2,4 @@ require_relative './company.rb'
 
 # This is your playground to test your class(es)!
 lewagon = Company.new("Le Wagon", "bootcamp", ["Mathier", "Ssaunier", "Roberto", "Fernando"])
+puts lewagon
