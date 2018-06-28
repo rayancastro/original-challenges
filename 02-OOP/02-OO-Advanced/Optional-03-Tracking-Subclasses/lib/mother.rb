f2d99d1f3ec4e45b24b9b5c53d2bb8956ea074e9
Rayan Castro <rayancdc@gmail.com>
2018-06-27 21:45:13 -0300

    Solve mother phone kids challenge

--- 02-OOP/02-OO-Advanced/Optional-03-Tracking-Subclasses/lib/mother.rb
+++ 02-OOP/02-OO-Advanced/Optional-03-Tracking-Subclasses/lib/mother.rb
@@ -1,4 +1,11 @@
 class Mother
+  def self.descendants
+    ObjectSpace.each_object(Class).select { |object| object < self }
+  end
+
+  def self.phone_kids
+    descendants.each { |element| element.phone }
+  end
 end
 
 # Daughter and Son are classes inheriting from Mother. They are defined
