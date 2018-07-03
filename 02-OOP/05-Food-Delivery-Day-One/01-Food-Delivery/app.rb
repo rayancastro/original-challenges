9946045ad7421c1228945bb45332db4f4f0448fc
Rayan Castro <rayancdc@gmail.com>
2018-07-03 11:10:21 -0300

    implement app, router and csv file

--- 02-OOP/05-Food-Delivery-Day-One/01-Food-Delivery/app.rb
+++ 02-OOP/05-Food-Delivery-Day-One/01-Food-Delivery/app.rb
@@ -1,3 +1,17 @@
 # TODO: require relevant files to bootstrap the app.
 # Then you can test your program with:
 #   ruby app.rb
+
+require_relative './app/controllers/meals_controller.rb'
+require_relative './app/models/meal.rb'
+require_relative './app/repositories/meal_repository'
+require_relative './app/views/view.rb'
+require_relative './router.rb'
+require 'csv'
+
+csv_file = File.join(__dir__, 'meals.csv')
+meal_repository = MealRepository.new(csv_file)
+meals_controller = MealsController.new(meal_repository)
+router = Router.new(meals_controller)
+
+router.run
--- 02-OOP/05-Food-Delivery-Day-One/01-Food-Delivery/router.rb
+++ 02-OOP/05-Food-Delivery-Day-One/01-Food-Delivery/router.rb
@@ -1,1 +1,40 @@
 # TODO: implement the router of your app.
+class Router
+  def initialize(controller)
+    @controller = controller
+    @running = true
+  end
+
+  def run
+    puts "Welcome"
+
+    while @running
+      display_tasks
+      action = gets.chomp.to_i
+      print `clear`
+      router_action(action)
+    end
+  end
+
+  def display_tasks
+    puts "1 - List all meals"
+    puts "2 - Add a meal"
+    puts "3 - Close program"
+  end
+
+  def router_action(action)
+    case action
+    when 1 then @controller.list
+    when 2 then @controller.add
+    when 3 then stop
+    else
+      puts "Please press a valid option number"
+    end
+  end
+
+  def stop
+    @running = false
+  end
+end
+
+
