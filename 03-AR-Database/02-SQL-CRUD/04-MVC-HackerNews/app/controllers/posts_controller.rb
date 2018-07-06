fec7ccbeb0417559a2b74e44be8d67139e32dee4
Rayan Castro <rayancdc@gmail.com>
2018-07-06 12:02:26 -0300

    Solve last challenge

--- 03-AR-Database/02-SQL-CRUD/04-MVC-HackerNews/app/controllers/posts_controller.rb
+++ 03-AR-Database/02-SQL-CRUD/04-MVC-HackerNews/app/controllers/posts_controller.rb
@@ -1,3 +1,5 @@
+require_relative '../models/post.rb'
+
 class PostsController
   def initialize
     @view = PostsView.new
@@ -9,21 +11,48 @@ class PostsController
 
   def index
     # TODO: implement listing all posts
+    @view.display_all(Post.all)
   end
 
   def create
     # TODO: implement creating a new post
+    title = @view.ask_title
+    post = Post.new(title: title)
+    post.save
   end
 
   def update
-    # TODO: implement updating an existing post
+    id = @view.ask_post_id_update
+    post = Post.find(id)
+    title = @view.ask_title_update(post)
+    url = @view.ask_url_update(post)
+    post.title = title unless title == ""
+    post.url = url unless url == ""
+    post.save
+    # votes = @view.ask_votes_update(post)
   end
 
+  # def generate_attributes(title, votes, url, id)
+  #   result = {}
+  #   result[:title] = title unless title == ""
+  #   result[:votes] = votes unless votes == 0
+  #   result[:url] = url unless url == ""
+  #   result[:id] = id if id
+  #   result
+  # end
+
   def destroy
-    # TODO: implement destroying a post
+    id = @view.ask_post_id_delete
+    post = Post.find(id)
+    check = @view.ask_confirmation(post)
+    post.destroy if check
   end
 
   def upvote
     # TODO: implement upvoting a post
+    id = @view.ask_post_id_upvote
+    post = Post.find(id)
+    post.votes += 1
+    post.save
   end
 end
--- 03-AR-Database/02-SQL-CRUD/04-MVC-HackerNews/app/models/post.rb
+++ 03-AR-Database/02-SQL-CRUD/04-MVC-HackerNews/app/models/post.rb
@@ -1,9 +1,47 @@
 class Post
-  # TODO: Gather all code from all previous exercises
-  # - reader and accessors
-  # - initialize
-  # - self.find
-  # - self.all
-  # - save
-  # - destroy
+  attr_accessor :title, :url, :votes
+  attr_reader :id
+  def initialize(attributes = {})
+    @id = attributes[:id]
+    @title = attributes[:title]
+    @url = attributes[:url]
+    @votes = attributes[:votes] || 0
+  end
+
+  def self.all
+    result = []
+    query = "SELECT * FROM posts"
+    DB.results_as_hash = true
+    DB.execute(query).each do |post|
+      result << Post.new(id: post['id'], title: post['title'], url: post['url'], votes: post['votes'])
+    end
+    return result
+  end
+
+  def self.find(id)
+    query = "SELECT id, title, url, votes FROM posts WHERE id = ?"
+    DB.results_as_hash = true
+    post = DB.execute(query, id).first
+    Post.new(id: post['id'], title: post['title'], url: post['url'], votes: post['votes']) if post
+  end
+
+  def destroy
+    query = "DELETE FROM posts WHERE id = ?"
+    DB.execute(query, @id)
+  end
+
+  def save
+    @id.nil? ? insert : update
+  end
+
+  def insert
+    query = "INSERT INTO posts (title, url, votes) VALUES (?, ?, ?)"
+    DB.execute(query, @title, @url, @votes)
+    @id = DB.last_insert_row_id
+  end
+
+  def update
+    query = "UPDATE posts SET title=?, url=?, votes=? WHERE id=?"
+    DB.execute(query, @title, @url, @votes, @id)
+  end
 end
