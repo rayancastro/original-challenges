9bcf801ad2b8f8d8f6090ebac93914da23320b8c
Rayan Castro <rayancdc@gmail.com>
2018-07-06 10:31:30 -0300

    solve read

--- 03-AR-Database/02-SQL-CRUD/01-Read/app/models/post.rb
+++ 03-AR-Database/02-SQL-CRUD/01-Read/app/models/post.rb
@@ -3,5 +3,29 @@
 # NO NEED TO CREATE IT, JUST USE IT.
 
 class Post
-  # TODO
+  attr_accessor :title, :url, :votes
+  attr_reader :id
+  def initialize(attributes = {})
+    @id = attributes[:id]
+    @title = attributes[:title]
+    @url = attributes[:url]
+    @votes = attributes[:votes]
+  end
+
+  def self.all
+    result = []
+    query = "SELECT * FROM posts"
+    DB.results_as_hash = true
+    DB.execute(query).each do |post|
+      result << Post.new(id: post['id'], title: post['title'], url: post['url'], votes: post['votes'])
+    end
+    return result
+  end
+
+  def self.find(id)
+    query = "SELECT id, title, url, votes FROM posts WHERE id = ?"
+    DB.results_as_hash = true
+    post = DB.execute(query, id).first
+    Post.new(id: post['id'], title: post['title'], url: post['url'], votes: post['votes']) if post
+  end
 end
