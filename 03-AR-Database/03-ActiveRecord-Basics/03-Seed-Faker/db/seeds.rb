5e46d93b3148ec0c5889fa3ea94b4709c19b101c
Rayan Castro <rayancdc@gmail.com>
2018-07-08 20:08:57 -0300

    Solve seeds

--- 03-AR-Database/03-ActiveRecord-Basics/03-Seed-Faker/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/03-Seed-Faker/db/seeds.rb
@@ -1,3 +1,16 @@
 require "faker"
-
 # TODO: Write a seed to insert 100 posts in the database
+N = 100
+puts "Loading unexistant inventory..."
+puts "Generating #{N} fake ids...."
+
+N.times do
+  post = Post.new(title: Faker::GameOfThrones.quote,
+                  url:   Faker::Internet.url,
+                  votes: rand(0..N))
+  post.save
+
+end
+puts "Finished!"
+
+
