1b552f57ff1a2bd93eab5e2883d740e97563a9e4
Rayan Castro <rayancdc@gmail.com>
2018-07-17 15:04:48 -0300

    Lord of the Rings done

--- 04-Front-End/05-JavaScript-DOM/04-Lord-of-the-rings/lib/lord_of_the_rings.js
+++ 04-Front-End/05-JavaScript-DOM/04-Lord-of-the-rings/lib/lord_of_the_rings.js
@@ -1,13 +1,43 @@
+const GOOD = ["Hobbits", "Elves", "Dwarves", "Eagles"];
+const EVIL = ["Orcs", "Wargs", "Goblins", "Uruk Hai", "Trolls"];
+
 function isGood(soldierType) {
   // TODO: return `true` if the soldierType is Good.
+  if (GOOD.includes(soldierType)) {
+    return true;
+  }
+  return false;
 }
 
 function buildSoldierMap(battlefield) {
   // TODO: Given a battlefield (String), return a Map of forces.
+  const races = battlefield.split(",");
+  const battle = new Map();
+  races.forEach((race) => {
+    const result = race.split(":");
+    battle.set(result[0], parseInt(result[1], 10));
+  });
+  return battle;
 }
 
 function whoWinsTheWar(battlefield) {
   // TODO: Based on the battlefield's description (it's a String), return "Good", "Evil" or "Tie".
+  const soldierMap = buildSoldierMap(battlefield);
+  let sumGood = 0;
+  let sumEvil = 0;
+  soldierMap.forEach((value, key) => {
+    if (isGood(key)) {
+      sumGood += value;
+    } else {
+      sumEvil += value;
+    }
+  });
+  if (sumGood > sumEvil) {
+    return "Good";
+  } else if (sumGood < sumEvil) {
+    return "Evil";
+  }
+  return "Tie";
 }
 
 module.exports = { whoWinsTheWar, buildSoldierMap, isGood }; // Do not remove this line
