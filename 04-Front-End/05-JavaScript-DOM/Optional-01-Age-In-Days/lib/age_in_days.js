0d88eb5ebc418a599ad8d82d58f3b257b8200863
Rayan Castro <rayancdc@gmail.com>
2018-07-17 16:24:26 -0300

    Solve age in days

--- 04-Front-End/05-JavaScript-DOM/Optional-01-Age-In-Days/lib/age_in_days.js
+++ 04-Front-End/05-JavaScript-DOM/Optional-01-Age-In-Days/lib/age_in_days.js
@@ -1,5 +1,7 @@
 function ageInDays(day, month, year) {
   // TODO: Implement the method and return an integer
+  const ageInMillis = Date.now() - new Date(year, month - 1, day);
+  return Math.round(ageInMillis / 86400000);
 }
 
 module.exports = ageInDays; // Do not remove. We need this for the spec to know about your method.
