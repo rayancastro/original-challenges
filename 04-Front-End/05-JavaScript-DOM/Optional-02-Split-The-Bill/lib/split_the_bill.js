eb81858fec81587d2607c7e076a233d076080ab8
Rayan Castro <rayancdc@gmail.com>
2018-07-17 17:10:39 -0300

    solve split bill problem

--- 04-Front-End/05-JavaScript-DOM/Optional-02-Split-The-Bill/lib/split_the_bill.js
+++ 04-Front-End/05-JavaScript-DOM/Optional-02-Split-The-Bill/lib/split_the_bill.js
@@ -1,5 +1,29 @@
+function convertObjectToMap(object) {
+  const map = new Map();
+  const entries = Object.entries(object);
+  entries.forEach((pair) => {
+    map.set(pair[0], pair[1]);
+  });
+  return map;
+}
+
 function splitTheBill(group) {
   // TODO: implement the method and return a hash
+  const values = Object.values(group);
+  const mappedGroup = convertObjectToMap(group);
+  let total = 0;
+  values.forEach((value) => {
+  // For each person, add the spent value to total
+    total += value;
+  });
+  // get the average from the total
+  const average = total / values.length;
+  // for each person, the total to receive is what he has spent - average
+  const result = {};
+  mappedGroup.forEach((value, key) => {
+    result[key] = value - average;
+  });
+  return result;
 }
 
 module.exports = splitTheBill; // Do not remove this line.
