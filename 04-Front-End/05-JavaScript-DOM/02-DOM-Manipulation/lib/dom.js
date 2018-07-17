d8f34d0e37182a4b0c334a0932d6c202e0244962
Rayan Castro <rayancdc@gmail.com>
2018-07-17 11:31:19 -0300

    append child plays

--- 04-Front-End/05-JavaScript-DOM/02-DOM-Manipulation/lib/dom.js
+++ 04-Front-End/05-JavaScript-DOM/02-DOM-Manipulation/lib/dom.js
@@ -25,29 +25,47 @@ module.exports = function runChallenges(check) {
 
   // Ex 3. Replace the email hint (next to the input) with 'This is my email now'
   //       The text should be emphasized using a <strong> tag
-
+  let emailHint = document.getElementById("email-hint");
+  emailHint.innerHTML = "This is my email now"
 
 
   // Ex 4. Add the .blue CSS class to the table header cells (th elements)
-
-
+  const tableHeader = document.querySelectorAll("th");
+  tableHeader.forEach((th) => {
+    th.classList.add("blue");
+  });
 
   // Ex 5. Count the number of table body rows there are
   //       Make the function countTeam() return it
   function countTeam() {
     // TODO: return the number of team
+    const tableRows = document.querySelectorAll("tbody tr");
+    return tableRows.length;
   }
 
 
   // Ex 6. Say there is a 15th team added to the table.
   //       Add a row at the bottom, this new team should have zero points.
-
-
+  let table = document.querySelector("tbody");
+  let newrow = table.appendChild(document.createElement('tr'));
+  let newrank = document.createElement('td')
+  let t = document.createTextNode("15");
+  newrank.appendChild(t);
+  let newname = document.createElement('td');
+  let t2 = document.createTextNode("The Unstoppable Chaves");
+  newname.appendChild(t2);
+  let newpoints = document.createElement('td')
+  let t3 = document.createTextNode("0");
+  newpoints.appendChild(t3);
+  newrow.appendChild(newrank);
+  newrow.appendChild(newname);
+  newrow.appendChild(newpoints);
 
   // Ex 7. Write some code to sum all points given to all teams
   //       Make the function summarizePoints() return it
   function summarizePoints() {
     // TODO: return the sum
+
   }
 
 
