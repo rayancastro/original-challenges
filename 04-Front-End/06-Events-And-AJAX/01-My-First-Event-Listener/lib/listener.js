a4afbe5189139eeded96a0e4c2fcfd499ec4d064
Rayan Castro <rayancdc@gmail.com>
2018-07-18 12:16:18 -0300

    added sound

--- 04-Front-End/06-Events-And-AJAX/01-My-First-Event-Listener/lib/listener.js
+++ 04-Front-End/06-Events-And-AJAX/01-My-First-Event-Listener/lib/listener.js
@@ -1,1 +1,15 @@
-// TODO: React to a click on the button!
+const audio = new Audio('../sound.mp3');
+
+const addSound = (event) => {
+  const target = event.currentTarget;
+  // Add disable class
+  target.classList.add("disable");
+  // Change text from click me to Bingo!
+  target.innerText = "Bingo!";
+  // Play mp3 sound in the browser
+  audio.play();
+};
+// Select blueButton from document
+const blueButton = document.getElementById("clickme");
+// Add listener when click on the blue button
+blueButton.addEventListener('click', addSound);
