57cf373028eeae5de961ecf03a79376c92461baa
Rayan Castro <rayancdc@gmail.com>
2018-07-18 16:21:13 -0300

    solve autocomplete

--- 04-Front-End/06-Events-And-AJAX/03-AJAX-Autocomplete/lib/autocomplete.js
+++ 04-Front-End/06-Events-And-AJAX/03-AJAX-Autocomplete/lib/autocomplete.js
@@ -1,1 +1,20 @@
+const results = document.getElementById("results");
+const input = document.getElementById("search");
+
 // TODO: Autocomplete the input with AJAX calls.
+
+const fetchAutoComplete = (event) => {
+  const fetchPath = `https://wagon-dictionary.herokuapp.com/autocomplete/${event.target.value}`;
+  results.innerHTML = "";
+  fetch(fetchPath).then(response => response.json()).then((data) => {
+  // Do something once HTTP response is received
+    data.words.forEach((word) => {
+      results.insertAdjacentHTML("beforeend", `<li>${word}</li>`);
+    });
+  });
+};
+
+// const urlAPI = "https://wagon-dictionary.herokuapp.com/autocomplete/ca"
+
+
+input.addEventListener("keyup", fetchAutoComplete);
