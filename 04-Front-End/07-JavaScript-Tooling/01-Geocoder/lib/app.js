fda3e10183fd286bd6cff509ac13cbc35a86f618
Rayan Castro <rayancdc@gmail.com>
2018-07-19 13:54:32 -0300

    done geocode and style

--- 04-Front-End/07-JavaScript-Tooling/01-Geocoder/lib/app.js
+++ 04-Front-End/07-JavaScript-Tooling/01-Geocoder/lib/app.js
@@ -1,1 +1,17 @@
-// TODO: Put your JS code in here
+import GMaps from 'gmaps';
+
+const myLatLng = { lat: 0, lng: 0 };
+const form = document.getElementById("form");
+const address = document.getElementById("address");
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  event.target.insertAdjacentHTML("afterend",'<div id="map" class="circle"></div>')
+  const URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${address.value}`;
+  fetch(URL).then(response => response.json()).then((data) => {
+    myLatLng.lat = data.results[0].geometry.location.lat;
+    myLatLng.lng = data.results[0].geometry.location.lng;
+    const map = new GMaps({ el: '#map', lat: myLatLng.lat, lng: myLatLng.lng, zoom: 14 });
+    const marker = { lat: myLatLng.lat, lng: myLatLng.lng };
+    map.addMarkers([marker]);
+  });
+});
