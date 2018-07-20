a076ce9e27143beefffcdb973f876e734cf3f68e
Rayan Castro <rayancdc@gmail.com>
2018-07-19 23:50:12 -0300

    Style changes

--- 04-Front-End/07-JavaScript-Tooling/02-Promo-Chat/lib/app.js
+++ 04-Front-End/07-JavaScript-Tooling/02-Promo-Chat/lib/app.js
@@ -1,43 +1,40 @@
 const batch = 158; // change to your own batch id
 const baseUrl = "https://wagon-chat.herokuapp.com/";
 
-const message = document.getElementById("your-message")
-const author = document.getElementById("your-name")
-const list = document.getElementById("comment-list")
-const refreshButton = document.getElementById("refresh")
-const form = document.getElementById("comment-form")
-const apiPath = `${baseUrl}${batch}/messages`
+const content = document.getElementById("your-message");
+const author = document.getElementById("your-name");
+const list = document.getElementById("comment-list");
+const refreshButton = document.getElementById("refresh");
+const form = document.getElementById("comment-form");
+const apiPath = `${baseUrl}${batch}/messages`;
 // Your turn to code!
 
 const showMessage = (event) => {
   event.preventDefault();
   const messageObject = {
-    "author": author.value,
-   "content": message.value
-  }
+    author: author.value,
+    content: content.value
+  };
 
-  list.insertAdjacentHTML("afterbegin", `<li>${message.value} (posted <span class="date">9 minutes ago</span>) by ${author.value}</li>`)
+  list.insertAdjacentHTML("afterbegin", `<li>${content.value} (posted <span class="date">9 minutes ago</span>) by ${author.value}</li>`);
   fetch(apiPath, {
     method: "POST",
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(messageObject)
   }).then(response => response.json()).then((data) => {
     console.log(data);
-  })
-}
+  });
+};
 
 const getMessages = (event) => {
   fetch(apiPath).then(response => response.json()).then((data) => {
     list.innerHTML = "";
     data.messages.forEach((message) => {
-      list.insertAdjacentHTML("afterbegin", `<li>${message.content} (posted <span class="date">${Math.floor((Date.now() - new Date(message.created_at)) / (1000 * 60))}</span> minutes ago) by ${message.author}</li>`)
-    })
-
-})
-
-}
+      list.insertAdjacentHTML("afterbegin", `<li>${message.content} (posted <span class="date">${Math.floor((Date.now() - new Date(message.created_at)) / (1000 * 60))}</span> minutes ago) by ${message.author}</li>`);
+    });
+  });
+};
 
 setInterval(getMessages, 1000);
 
-form.addEventListener("submit", showMessage)
-refreshButton.addEventListener("click", getMessages)
+form.addEventListener("submit", showMessage);
